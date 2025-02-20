7f7ca7c3aad7d4ed54bf87942b24b9f7658a89e2
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-02-21 00:41:25 +0530

    user added in UI and button to change it

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  onSelectedUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 }
