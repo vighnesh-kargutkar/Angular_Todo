e7fd4892106a8af0f132db8b75c9a1d646f35dd3
vighnesh-kargutkar <kargutkar860@gmail.com>
2025-02-21 10:50:46 +0530

    fetching values using input decorater

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,10 +7,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex])
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  @Input() avatar!: string
+  @Input() name!: string
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
   onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+
   }
 }
