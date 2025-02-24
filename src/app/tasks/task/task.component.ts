3d1e047a21e5f2b7398fe2b4849feeeae1b041bf
vighnesh Kargutkar <kargutkar860@gmail.com>
2025-02-25 00:13:21 +0530

    styyling done

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { type Task } from './task.model';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [ DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { CardComponent } from '../shared/card/card.component'
 import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
